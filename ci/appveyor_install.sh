e4c2e0cf6ed16269fd2f77fc111f9e8ebe051930
oddcoder <ahmedsoliman@oddcoder.com>
2017-04-28 22:59:18 +0200

    attempt 2

--- ci/appveyor_install.sh
+++ ci/appveyor_install.sh
@@ -3,3 +3,4 @@ curl http://bin.rada.re/radare2-w32-1.4.0.zip > radare2-w32-1.4.0.zip
 unzip radare2-w32-1.4.0.zip
 export LIBRARY_PATH=$(pwd)/radare2-w32-1.4.0
 cargo build --release --verbose
+cp radare2-w32-1.4.0/*.dll target/release/
