795eea2d746441e4b22feba1cf2176022c3ee2fa
oddcoder <ahmedsoliman@oddcoder.com>
2019-10-09 23:46:14 +0200

    Add code Coverage

--- ci/test.sh
+++ ci/test.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
-
-cargo test --all
+cargo test --all --no-run
+cargo test --all --doc
+wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
+tar xzf master.tar.gz &&
+cd kcov-master &&
+mkdir build &&
+cd build &&
+cmake .. &&
+make &&
+make install DESTDIR=../../kcov-build &&
+cd ../.. &&
+rm -rf kcov-master &&
+for file in target/debug/*; do
+  [ -x "${file}" ] || continue;
+  mkdir -p "target/cov/$(basename $file)";
+  ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
+done
+curl -s https://codecov.io/bash | bash &&
+echo "Uploaded code coverage"
