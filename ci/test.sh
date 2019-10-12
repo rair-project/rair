d852142a04cbbaf1c554e857b120bb3e155aa9a0
oddcoder <ahmedsoliman@oddcoder.com>
2019-10-12 20:29:15 +0200

    Enable Index Page

--- ci/test.sh
+++ ci/test.sh
@@ -11,9 +11,9 @@ fi
 if [ $DOC ]; then
   cargo install cargo-deadlinks
   cargo doc  --no-deps --all # building docs
+  cargo rustdoc -- -Z unstable-options --enable-index-page
   cargo deadlinks --check-http --dir target/doc #testing docs
   pip install ghp-import --user
-  echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html
   ghp-import -n target/doc
   git push -fq https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 fi
