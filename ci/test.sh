0592cf49836c6da7ae3ddffecf3de839ceed0fd1
oddcoder <ahmedsoliman@oddcoder.com>
2019-10-12 13:18:40 +0200

    Using tarpaulin a rust suited code coverage instead of kcov

--- ci/test.sh
+++ ci/test.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
-cargo test --all --no-run
-cargo test --all --doc
-wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
-tar xzf master.tar.gz &&
-cd kcov-master &&
-mkdir build &&
-cd build &&
-cmake .. &&
-make &&
-make install DESTDIR=../../kcov-build &&
-cd ../.. &&
-rm -rf kcov-master &&
-for file in target/debug/*; do
-  [ -x "${file}" ] || continue;
-  mkdir -p "target/cov/$(basename $file)";
-  ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file";
-done
-curl -s https://codecov.io/bash | bash &&
-echo "Uploaded code coverage"
+if [ $DOCOV ]; then
+  cargo install cargo-tarpaulin
+  cargo test --all --doc
+  cargo tarpaulin --ignore-tests -l --all --out Xml
+  curl -s https://codecov.io/bash | bash
+else
+  cargo test --all
+fi
