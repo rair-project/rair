fd73c0857e84caed1be3480918dcee96bd4384e8
oddcoder <ahmedsoliman@oddcoder.com>
2019-10-12 20:22:00 +0200

    Generate all local docs

--- ci/test.sh
+++ ci/test.sh
@@ -10,7 +10,7 @@ fi
 
 if [ $DOC ]; then
   cargo install cargo-deadlinks
-  cargo doc  # building docs
+  cargo doc  --no-deps --all # building docs
   cargo deadlinks --check-http --dir target/doc #testing docs
   pip install ghp-import --user
   echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html
