8ebdaebe26a2d70250cc462c2286211e5e1e7911
oddcoder <ahmedsoliman@oddcoder.com>
2019-10-19 00:19:15 +0200

    Make CI fail if any command failed

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+set -e
+
 if [ $COV ]; then
+  set +e # cargo install might fail because package already exist in cache
   cargo install cargo-tarpaulin
+  set -e
   cargo test --all --doc # testing doc tests
   cargo tarpaulin --ignore-tests -l --all --out Xml # testing and code coveage
   curl -s https://codecov.io/bash | bash
@@ -9,7 +13,9 @@ else
 fi
 
 if [ $DOC ]; then
-  cargo install cargo-deadlinks
+  set +e # cargo install might fail because package already exist in cache
+  cargo install cargo-deadlinks -q
+  set -e
   cargo doc  --no-deps --all # building docs
   cargo rustdoc -- -Z unstable-options --enable-index-page
   cargo deadlinks --check-http --dir target/doc #testing docs
