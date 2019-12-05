bef48266bfa481b23cde97ff45ee8dba795affc6
oddcoder <ahmedsoliman@oddcoder.com>
2019-12-05 18:59:05 +0200

    Add Command to open file

--- rcore/src/io/files.rs
+++ rcore/src/io/files.rs
@@ -18,6 +18,7 @@
 use core::*;
 use helper::*;
 use std::io::Write;
+use rio::*;
 
 #[derive(Default)]
 pub struct ListFiles {}
@@ -30,7 +31,7 @@ impl ListFiles {
 
 impl Cmd for ListFiles {
     fn run(&mut self, core: &mut Core, args: &[String]) {
-        if args.len() != 0 {
+        if !args.is_empty() {
             expect(core, args.len() as u64, 0);
             return;
         }
@@ -38,7 +39,7 @@ impl Cmd for ListFiles {
         for file in core.io.uri_iter() {
             let perm = format!("{:?}", file.perm());
             write!(core.stdout, "{}\t0x{:08x}\t0x{:08x}\t{}", file.hndl(), file.paddr_base(), file.size(), perm).unwrap();
-            if perm.len() < 6{
+            if perm.len() < 6 {
                 write!(core.stdout, "\t").unwrap();
             }
             writeln!(core.stdout, "\t{}", file.name()).unwrap();
@@ -46,5 +47,84 @@ impl Cmd for ListFiles {
     }
     fn help(&self, core: &mut Core) {
         help(core, &"files", &"", vec![("", "List all open files.")]);
+    }
+}
+
+#[derive(Default)]
+pub struct OpenFile {}
+
+impl OpenFile {
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+fn parse_perm(p: &str) -> Result<IoMode,String> {
+    let mut perm = Default::default();
+    for c in p.to_lowercase().chars() {
+        match c {
+            'r' => perm |= IoMode::READ,
+            'w' => perm |= IoMode::WRITE,
+            'c' => perm |= IoMode::COW,
+            _ => return Err(format!("Unknown Permission: `{}`", c)),
+        }
+    }
+    return Ok(perm);
+}
+impl Cmd for OpenFile {
+    fn run(&mut self, core: &mut Core, args: &[String]) {
+        if args.len() > 3 || args.len() < 1 {
+            expect_range(core, args.len() as u64, 1, 2);
+            return;
+        }
+        let uri;
+        let perm;
+        let addr;
+        if args.len() == 3 {
+            uri = &args[1];
+            perm = match parse_perm(&args[0]) {
+                Ok(perm) => perm,
+                Err(e) => return error_msg(core, "Failed to parse permission",&e),
+            };
+            addr = match str_to_num(&args[2]) {
+                Ok(addr) => Some(addr),
+                Err(e) => {
+                    let err_str = format!("{}", e);
+                    error_msg(core, "Failed to parse address", &err_str);
+                    return;
+                }
+            }
+        }
+        else if args.len() == 2 {
+            if let Ok(a) = str_to_num(&args[1]) {
+                addr = Some(a);
+                uri = &args[0];
+                perm = IoMode::READ;
+            } else {
+                uri = &args[1];
+                perm = match parse_perm(&args[0]) {
+                    Ok(perm) => perm,
+                    Err(e) => return error_msg(core, "Failed to parse permission",&e),
+                };
+                addr = None;
+            }
+                
+        } else {
+            uri = &args[0];
+            perm = IoMode::READ;
+            addr = None;
+        }
+
+        let result = match addr {
+            Some(addr) => core.io.open_at(uri, perm, addr),
+            None => core.io.open(uri, perm),
+        };
+        if let Err(e) = result {
+            let err_str = format!("{}", e);
+            error_msg(core, "Failed to Open File", &err_str);
+        }
+
+    }
+    fn help(&self, core: &mut Core) {
+        help(core, &"open", &"o", vec![("<Perm> [URI] <Addr>", "Open given URI using given optional permission (default to readonly) at given optional address")]);
     }
 }
