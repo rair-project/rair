7a3cc1480a8ad1dc07465e8d21164b748b45aa89
oddcoder <ahmedsoliman@oddcoder.com>
2019-12-06 19:49:21 +0200

    Test files commands

--- rcore/src/io/files.rs
+++ rcore/src/io/files.rs
@@ -17,8 +17,8 @@
 
 use core::*;
 use helper::*;
-use std::io::Write;
 use rio::*;
+use std::io::Write;
 
 #[derive(Default)]
 pub struct ListFiles {}
@@ -58,7 +58,7 @@ impl OpenFile {
         Default::default()
     }
 }
-fn parse_perm(p: &str) -> Result<IoMode,String> {
+fn parse_perm(p: &str) -> Result<IoMode, String> {
     let mut perm = Default::default();
     for c in p.to_lowercase().chars() {
         match c {
@@ -72,18 +72,18 @@ fn parse_perm(p: &str) -> Result<IoMode,String> {
 }
 impl Cmd for OpenFile {
     fn run(&mut self, core: &mut Core, args: &[String]) {
-        if args.len() > 3 || args.len() < 1 {
+        if args.len() > 3 || args.is_empty() {
             expect_range(core, args.len() as u64, 1, 2);
             return;
         }
         let uri;
-        let perm;
-        let addr;
+        let mut perm = IoMode::READ;
+        let mut addr = None;
         if args.len() == 3 {
             uri = &args[1];
             perm = match parse_perm(&args[0]) {
                 Ok(perm) => perm,
-                Err(e) => return error_msg(core, "Failed to parse permission",&e),
+                Err(e) => return error_msg(core, "Failed to parse permission", &e),
             };
             addr = match str_to_num(&args[2]) {
                 Ok(addr) => Some(addr),
@@ -93,25 +93,19 @@ impl Cmd for OpenFile {
                     return;
                 }
             }
-        }
-        else if args.len() == 2 {
+        } else if args.len() == 2 {
             if let Ok(a) = str_to_num(&args[1]) {
                 addr = Some(a);
                 uri = &args[0];
-                perm = IoMode::READ;
             } else {
                 uri = &args[1];
                 perm = match parse_perm(&args[0]) {
                     Ok(perm) => perm,
-                    Err(e) => return error_msg(core, "Failed to parse permission",&e),
+                    Err(e) => return error_msg(core, "Failed to parse permission", &e),
                 };
-                addr = None;
             }
-                
         } else {
             uri = &args[0];
-            perm = IoMode::READ;
-            addr = None;
         }
 
         let result = match addr {
@@ -120,18 +114,21 @@ impl Cmd for OpenFile {
         };
         if let Err(e) = result {
             let err_str = format!("{}", e);
-            error_msg(core, "Failed to Open File", &err_str);
+            error_msg(core, "Failed to open file", &err_str);
         }
-
     }
     fn help(&self, core: &mut Core) {
-        help(core, &"open", &"o", vec![("<Perm> [URI] <Addr>", "Open given URI using given optional permission (default to readonly) at given optional address.")]);
+        help(
+            core,
+            &"open",
+            &"o",
+            vec![("<Perm> [URI] <Addr>", "Open given URI using given optional permission (default to readonly) at given optional address.")],
+        );
     }
 }
 
 #[derive(Default)]
 pub struct CloseFile {}
-
 
 impl CloseFile {
     pub fn new() -> Self {
@@ -145,23 +142,135 @@ impl Cmd for CloseFile {
             expect(core, args.len() as u64, 1);
             return;
         }
-        let hndl =  match str_to_num(&args[0]){
+        let hndl = match str_to_num(&args[0]) {
             Ok(hndl) => hndl,
             Err(e) => {
                 let err_str = format!("{}", e);
                 error_msg(core, "Invalid hndl", &err_str);
                 return;
-
             }
         };
         if let Err(e) = core.io.close(hndl) {
             let err_str = format!("{}", e);
-            error_msg(core, "Failed to Open File", &err_str);
+            error_msg(core, "Failed to close file", &err_str);
         }
-
     }
     fn help(&self, core: &mut Core) {
         help(core, &"close", &"", vec![("[hndl]", "Close file with given hndl.")]);
     }
 }
 
+#[cfg(test)]
+mod test_files {
+    use super::*;
+    use writer::Writer;
+    use yansi::Paint;
+    #[test]
+    fn test_docs() {
+        Paint::disable();
+        let mut core = Core::new();
+        core.stderr = Writer::new_buf();
+        core.stdout = Writer::new_buf();
+        let files = ListFiles::new();
+        let open = OpenFile::new();
+        let close = CloseFile::new();
+        files.help(&mut core);
+        open.help(&mut core);
+        close.help(&mut core);
+        assert_eq!(
+            core.stdout.utf8_string().unwrap(),
+            "Command: [files]\n\n\
+             Usage:\n\
+             files\tList all open files.\n\
+             Commands: [open | o]\n\n\
+             Usage:\n\
+             o <Perm> [URI] <Addr>\tOpen given URI using given optional permission (default to readonly) at given optional address.\n\
+             Command: [close]\n\n\
+             Usage:\n\
+             close [hndl]\tClose file with given hndl.\n"
+        );
+        assert_eq!(core.stderr.utf8_string().unwrap(), "");
+    }
+
+    #[test]
+    fn test_open_close_files() {
+        Paint::disable();
+        let mut core = Core::new();
+        core.stderr = Writer::new_buf();
+        core.stdout = Writer::new_buf();
+        let mut files = ListFiles::new();
+        let mut open = OpenFile::new();
+        let mut close = CloseFile::new();
+        open.run(&mut core, &["b64://../../testing_binaries/rio/base64/no_padding.b64".to_string()]);
+        open.run(&mut core, &["rw".to_string(), "malloc://0x50".to_string()]);
+        open.run(&mut core, &["c".to_string(), "../../testing_binaries/rio/base64/one_pad.b64".to_string(), "0x5000".to_string()]);
+        open.run(&mut core, &["b64://../../testing_binaries/rio/base64/no_padding.b64".to_string(), "0xa000".to_string()]);
+
+        files.run(&mut core, &[]);
+        assert_eq!(
+            core.stdout.utf8_string().unwrap(),
+            "Handle\tStart address\tsize\t\tPermissions\tURI\n\
+             0\t0x00000000\t0x0000002d\tREAD\t\tb64://../../testing_binaries/rio/base64/no_padding.b64\n\
+             1\t0x0000002d\t0x00000050\tWRITE | READ\tmalloc://0x50\n\
+             2\t0x00005000\t0x00000005\tCOW\t\t../../testing_binaries/rio/base64/one_pad.b64\n\
+             3\t0x0000a000\t0x0000002d\tREAD\t\tb64://../../testing_binaries/rio/base64/no_padding.b64\n"
+        );
+        assert_eq!(core.stderr.utf8_string().unwrap(), "");
+        core.stderr = Writer::new_buf();
+        core.stdout = Writer::new_buf();
+        close.run(&mut core, &["1".to_string()]);
+        files.run(&mut core, &[]);
+        assert_eq!(
+            core.stdout.utf8_string().unwrap(),
+            "Handle\tStart address\tsize\t\tPermissions\tURI\n\
+             0\t0x00000000\t0x0000002d\tREAD\t\tb64://../../testing_binaries/rio/base64/no_padding.b64\n\
+             2\t0x00005000\t0x00000005\tCOW\t\t../../testing_binaries/rio/base64/one_pad.b64\n\
+             3\t0x0000a000\t0x0000002d\tREAD\t\tb64://../../testing_binaries/rio/base64/no_padding.b64\n"
+        );
+        assert_eq!(core.stderr.utf8_string().unwrap(), "");
+    }
+
+    #[test]
+    fn test_failing_parsing() {
+        Paint::disable();
+        let mut core = Core::new();
+        core.stderr = Writer::new_buf();
+        core.stdout = Writer::new_buf();
+        let mut open = OpenFile::new();
+        open.run(&mut core, &["z".to_string(), "malloc://0x50".to_string()]);
+        open.run(&mut core, &["z".to_string(), "malloc://0x50".to_string(), "0x500".to_string()]);
+        open.run(&mut core, &["rw".to_string(), "malloc://0x50".to_string(), "0b500".to_string()]);
+
+        assert_eq!(core.stdout.utf8_string().unwrap(), "");
+        assert_eq!(
+            core.stderr.utf8_string().unwrap(),
+            "Error: Failed to parse permission\n\
+             Unknown Permission: `z`\n\
+             Error: Failed to parse permission\n\
+             Unknown Permission: `z`\n\
+             Error: Failed to parse address\n\
+             invalid digit found in string\n"
+        );
+    }
+
+    #[test]
+    fn test_arguments_count() {
+        Paint::disable();
+        let mut core = Core::new();
+        core.stderr = Writer::new_buf();
+        core.stdout = Writer::new_buf();
+        let mut files = ListFiles::new();
+        let mut open = OpenFile::new();
+        let mut close = CloseFile::new();
+        open.run(&mut core, &[]);
+        files.run(&mut core, &["test".to_string()]);
+        close.run(&mut core, &[]);
+        assert_eq!(core.stdout.utf8_string().unwrap(), "");
+        assert_eq!(
+            core.stderr.utf8_string().unwrap(),
+            "Arguments Error: Expected between 1 and 2 arguments, found 0.\n\
+             Arguments Error: Expected 0 argument(s), found 1.\n\
+             Arguments Error: Expected 1 argument(s), found 0.\n"
+        );
+    }
+}
