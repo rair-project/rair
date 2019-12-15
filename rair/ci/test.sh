043e4f1500657046f7b08bb25c9653a4040c3dc6
oddcoder <ahmedsoliman@oddcoder.com>
2019-12-15 20:10:26 +0200

    Add CI support

---
+++ rair/ci/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+git -C ../ clone https://github.com/Rair-Project/rair-core.git
+
+if [ $COV ]; then
+  set +e # cargo install might fail because package already exist in cache
+  cargo install cargo-tarpaulin
+  set -e
+  cargo test --all --doc # testing doc tests
+  cargo tarpaulin --ignore-tests -l --all --out Xml # testing and code coveage
+  curl -s https://codecov.io/bash | bash
+else
+  cargo test --all
+fi
