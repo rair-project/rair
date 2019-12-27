4cc49d92c2636d8dd695ba27944fc0825ada7165
oddcoder <ahmedsoliman@oddcoder.com>
2019-12-27 16:54:35 +0200

    Fixing typo in command name (capitalization of first character)

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -34,9 +34,9 @@ pub fn register_io(core: &mut Core) {
     core.add_command("map", "", Rc::new(RefCell::new(Map::new())));
     core.add_command("maps", "", maps);
     core.add_command("printHex", "px", Rc::new(RefCell::new(PrintHex::new())));
-    core.add_command("PrintBase", "pb", Rc::new(RefCell::new(PrintBase::new())));
-    core.add_command("PrintCSV", "pcsv", Rc::new(RefCell::new(PrintCSV::new())));
-    core.add_command("PrintSignedCSV", "pscsv", Rc::new(RefCell::new(PrintSignedCSV::new())));
+    core.add_command("printBase", "pb", Rc::new(RefCell::new(PrintBase::new())));
+    core.add_command("printCSV", "pcsv", Rc::new(RefCell::new(PrintCSV::new())));
+    core.add_command("printSignedCSV", "pscsv", Rc::new(RefCell::new(PrintSignedCSV::new())));
     core.add_command("unmap", "um", Rc::new(RefCell::new(UnMap::new())));
     core.add_command("files", "", files);
     core.add_command("open", "o", Rc::new(RefCell::new(OpenFile::new())));
