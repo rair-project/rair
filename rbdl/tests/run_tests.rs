583221caccfd9ef61b6b2e14d39b39e2ba51af4a
oddcoder <ahmedsoliman@oddcoder.com>
2020-01-17 21:22:19 +0200

    killing tests with OS specific error messages

--- rbdl/tests/run_tests.rs
+++ rbdl/tests/run_tests.rs
@@ -2,7 +2,6 @@
 fn tests() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/not_str.rs");
-    t.compile_fail("tests/not_found.rs");
     t.compile_fail("tests/binary.rs");
     t.pass("tests/demo.rs");
 }
