458562c1cfcf7fb1ece78599536c6bc71f42ef47
oddcoder <ahmedsoliman@oddcoder.com>
2020-01-18 21:44:52 +0200

    Testing RBDLValue

--- rbdl/rbdl-syn/src/value.rs
+++ rbdl/rbdl-syn/src/value.rs
@@ -20,6 +20,7 @@ use syn::parse::{Parse, ParseStream};
 use syn::token::Bracket;
 use syn::{Ident, Lit, LitBool, LitByte, LitByteStr, LitChar, LitFloat, LitInt, LitStr, Result};
 
+// Possible Values to be used inside an attribute
 #[derive(Debug)]
 pub enum RBDLValue {
     Str(LitStr),
@@ -40,22 +41,22 @@ impl RBDLValue {
     }
 
     fn parse_lit(input: ParseStream) -> Result<Self> {
+        let head = input.fork();
         if input.peek(Lit) {
-            let lit = match input.parse::<Lit>()? {
-                Lit::Str(s) => RBDLValue::Str(s),
-                Lit::ByteStr(bs) => RBDLValue::ByteStr(bs),
-                Lit::Byte(b) => RBDLValue::Byte(b),
-                Lit::Char(c) => RBDLValue::Char(c),
-                Lit::Int(i) => RBDLValue::Int(i),
-                Lit::Float(f) => RBDLValue::Float(f),
-                Lit::Bool(b) => RBDLValue::Bool(b),
-                _ => panic!("Unknown literal type"),
-            };
-            return Ok(lit);
+            match input.parse::<Lit>()? {
+                Lit::Str(s) => Ok(RBDLValue::Str(s)),
+                Lit::ByteStr(bs) => Ok(RBDLValue::ByteStr(bs)),
+                Lit::Byte(b) => Ok(RBDLValue::Byte(b)),
+                Lit::Char(c) => Ok(RBDLValue::Char(c)),
+                Lit::Int(i) => Ok(RBDLValue::Int(i)),
+                Lit::Float(f) => Ok(RBDLValue::Float(f)),
+                Lit::Bool(b) => Ok(RBDLValue::Bool(b)),
+                _ => Err(head.error("Unknown literal type")),
+            }
         } else if input.peek(Ident) {
-            return Ok(RBDLValue::Ident(input.parse::<Ident>()?));
+            Ok(RBDLValue::Ident(input.parse::<Ident>()?))
         } else {
-            panic!("Expected Idenitifier or Literal.");
+            Err(head.error("Expected Idenitifier or Literal."))
         }
     }
 }
@@ -66,5 +67,105 @@ impl Parse for RBDLValue {
         } else {
             RBDLValue::parse_lit(input)
         }
+    }
+}
+
+#[cfg(test)]
+mod test_rbdl_syn_value {
+    use super::*;
+    use syn::parse_str;
+    #[test]
+    fn test_value_str() {
+        let val: RBDLValue = parse_str("\"Hello world\"").unwrap();
+        if let RBDLValue::Str(s) = val {
+            assert_eq!(s.value(), "Hello world");
+        } else {
+            panic!("Failed to parse string literal");
+        }
+    }
+    #[test]
+    fn test_value_byte_str() {
+        let val: RBDLValue = parse_str("b\"Hello world\"").unwrap();
+        if let RBDLValue::ByteStr(s) = val {
+            assert_eq!(s.value(), b"Hello world");
+        } else {
+            panic!("Failed to parse byte string literal");
+        }
+    }
+
+    #[test]
+    fn test_value_byte() {
+        let val: RBDLValue = parse_str("b'H'").unwrap();
+        if let RBDLValue::Byte(s) = val {
+            assert_eq!(s.value(), b'H');
+        } else {
+            panic!("Failed to parse byte");
+        }
+    }
+    #[test]
+    fn test_value_char() {
+        let val: RBDLValue = parse_str("'H'").unwrap();
+        if let RBDLValue::Char(s) = val {
+            assert_eq!(s.value(), 'H');
+        } else {
+            panic!("Failed to parse char");
+        }
+    }
+
+    #[test]
+    fn test_value_int() {
+        let val: RBDLValue = parse_str("1234").unwrap();
+        if let RBDLValue::Int(s) = val {
+            assert_eq!(1234, s.base10_parse().unwrap());
+        } else {
+            panic!("Failed to parse Int type");
+        }
+    }
+    #[test]
+    fn test_value_float() {
+        let val: RBDLValue = parse_str("1234.4321").unwrap();
+        if let RBDLValue::Float(s) = val {
+            assert_eq!(1234.4321, s.base10_parse().unwrap());
+        } else {
+            panic!("Failed to parse Float type");
+        }
+    }
+    #[test]
+    fn test_value_bool() {
+        let val: RBDLValue = parse_str("false").unwrap();
+        if let RBDLValue::Bool(s) = val {
+            assert_eq!(false, s.value);
+        } else {
+            panic!("Failed to parse Bool type");
+        }
+    }
+    #[test]
+    fn test_value_ident() {
+        let val: RBDLValue = parse_str("bla").unwrap();
+        if let RBDLValue::Ident(s) = val {
+            assert_eq!(s, "bla");
+        } else {
+            panic!("Failed to parse Bool type");
+        }
+    }
+    #[test]
+    fn test_value_vec() {
+        let val: RBDLValue = parse_str("[0, 1, 2 , 3, 4]").unwrap();
+        if let RBDLValue::Vec(vec) = val {
+            assert_eq!(vec.content.len(), 5);
+            for (i, int_val) in vec.content.iter().enumerate() {
+                if let RBDLValue::Int(int) = int_val {
+                    assert_eq!(i, int.base10_parse().unwrap());
+                } else {
+                    panic!("Failed to parse Vec type");
+                }
+            }
+        } else {
+            panic!("Failed to parse Vec type");
+        }
+    }
+    #[test]
+    fn test_error() {
+        assert!(parse_str::<RBDLValue>("+").is_err());
     }
 }
